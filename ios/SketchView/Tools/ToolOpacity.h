ae81ce31253ed45820a593e9d0a223030f6b9abf
Hiep Tran <hiep.tran@saigontechnology.vn>
2019-07-04 10:20:49 +0700

    Add Tool Opacity

---
+++ ios/SketchView/Tools/ToolOpacity.h
@@ -0,0 +1,21 @@
+//
+//  ToolColor.h
+//  Sketch
+//
+//  Created by Keshav on 10/04/17.
+//  Copyright © 2017 Particle41. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#ifndef ToolOpacity_h
+#define ToolOpacity_h
+
+#endif /* ToolOpacity_h */
+
+@protocol ToolOpacity <NSObject>
+
+- (void)setToolOpacity:(CGFloat)opacity;
+- (CGFloat)getToolOpacity;
+
+@end
